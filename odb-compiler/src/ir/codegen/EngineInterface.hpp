1c23eca4f3e68392a1f401383010a6c457395b28
David Avedissian <git@dga.dev>
2021-01-25 22:34:18 +0000

    Initial code generation implementation with TGC backend.

---
+++ odb-compiler/src/ir/codegen/EngineInterface.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "LLVM.hpp"
+#include "odb-compiler/commands/Command.hpp"
+
+namespace odb::ir {
+class EngineInterface
+{
+public:
+    explicit EngineInterface(llvm::Module& module) : module(module), ctx(module.getContext()) {}
+    virtual ~EngineInterface() = default;
+
+    virtual llvm::Function* generateCommandCall(const cmd::Command& command, const std::string& functionName,
+                                                llvm::FunctionType* functionType) = 0;
+    virtual void generateEntryPoint(llvm::Function* gameEntryPoint, std::vector<DynamicLibrary*> pluginsToLoad) = 0;
+
+protected:
+    llvm::Module& module;
+    llvm::LLVMContext& ctx;
+};
+} // namespace odb::ir
