50982c5e9a81ada5c480cf7b510b86352bbb6d86
TheComet <alex.murray@gmx.ch>
2021-02-17 19:36:18 +0100

    Adding AFL data for fuzzing

---
+++ scripts/fuzz_odbc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MEMORY=150
+TESTCASES=../../afl/testcases
+DICTIONARY=../../afl/dictionary/dbp.dict
+FINDINGS=afl-findings
+ODBC=./odbc
+
+afl-fuzz -M fuzzer1 -m "$MEMORY" -i "$TESTCASES" -x "$DICTIONARY" -o "$FINDINGS" "$ODBC" -n --parse-dba @@ &
+MASTERPID=$!
+for i in $(seq 2 $(($(nproc)*2)))
+do
+    afl-fuzz -S "fuzzer$i" -m "$MEMORY" -i "$TESTCASES" -x "$DICTIONARY" -o "$FINDINGS" "$ODBC" -n --parse-dba @@ 2>&1 > /dev/null &
+done
+wait $MASTERPID
+
