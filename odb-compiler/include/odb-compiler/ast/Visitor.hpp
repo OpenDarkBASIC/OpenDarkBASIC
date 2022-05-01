0900e4d370aeeb478601fd24c0b90fa7378e416a
David Avedissian <git@dga.dev>
2022-05-01 13:44:27 +0100

    Auto-generate visitor.

--- odb-compiler/include/odb-compiler/ast/Visitor.hpp
+++ odb-compiler/include/odb-compiler/ast/Visitor.hpp
@@ -2,56 +2,14 @@
 
 #include "odb-compiler/config.hpp"
 #include "odb-compiler/ast/Type.hpp"
+#include "odb-compiler/ast/Nodes.hpp"
 
 namespace odb::ast {
 
 class Node;
 
-class ArgList;
-class ArrayAssignment;
-class ArrayDecl;
-class ArrayRef;
-class BinaryOp;
-class Block;
-class Case;
-class CaseList;
-class Exit;
-class CommandExpr;
-class CommandStmnt;
-class Conditional;
-class ConstDecl;
-class ConstDeclExpr;
-class DefaultCase;
-class ForLoop;
-class FuncCallExpr;
-class FuncCallExprOrArrayRef;
-class FuncCallStmnt;
-class FuncDecl;
-class FuncExit;
-class Goto;
-class Identifier;
-class InfiniteLoop;
-class InitializerList;
-class Label;
-class ScopedIdentifier;
-class Select;
-class SubCall;
-class SubReturn;
-class VarDecl;
-class UDTDecl;
-class UDTDeclBody;
-class UDTFieldOuter;
-class UDTFieldInner;
-class UDTFieldAssignment;
-class UnaryOp;
-class UntilLoop;
-class VarAssignment;
-class VarRef;
-class WhileLoop;
-
-#define X(dbname, cppname) \
-    class dbname##Literal;
-ODB_DATATYPE_LIST
+#define X(nodeType) class nodeType;
+ODB_AST_NODE_TYPE_LIST
 #undef X
 
 class ODBCOMPILER_PUBLIC_API Visitor
@@ -59,51 +17,8 @@ class ODBCOMPILER_PUBLIC_API Visitor
 public:
     virtual ~Visitor() = default;
 
-    virtual void visitArgList(ArgList* node) = 0;
-    virtual void visitArrayAssignment(ArrayAssignment* node) = 0;
-    virtual void visitArrayDecl(ArrayDecl* node) = 0;
-    virtual void visitArrayRef(ArrayRef* node) = 0;
-    virtual void visitBinaryOp(BinaryOp* node) = 0;
-    virtual void visitBlock(Block* node) = 0;
-    virtual void visitCase(Case* node) = 0;
-    virtual void visitCaseList(CaseList* node) = 0;
-    virtual void visitCommandExpr(CommandExpr* node) = 0;
-    virtual void visitCommandStmnt(CommandStmnt* node) = 0;
-    virtual void visitConditional(Conditional* node) = 0;
-    virtual void visitConstDecl(ConstDecl* node) = 0;
-    virtual void visitConstDeclExpr(ConstDeclExpr* node) = 0;
-    virtual void visitDefaultCase(DefaultCase* node) = 0;
-    virtual void visitExit(Exit* node) = 0;
-    virtual void visitForLoop(ForLoop* node) = 0;
-    virtual void visitFuncCallExpr(FuncCallExpr* node) = 0;
-    virtual void visitFuncCallExprOrArrayRef(FuncCallExprOrArrayRef* node)  = 0;
-    virtual void visitFuncCallStmnt(FuncCallStmnt* node) = 0;
-    virtual void visitFuncDecl(FuncDecl* node) = 0;
-    virtual void visitFuncExit(FuncExit* node) = 0;
-    virtual void visitGoto(Goto* node) = 0;
-    virtual void visitIdentifier(Identifier* node) = 0;
-    virtual void visitInfiniteLoop(InfiniteLoop* node) = 0;
-    virtual void visitInitializerList(InitializerList* node) = 0;
-    virtual void visitLabel(Label* node) = 0;
-    virtual void visitScopedIdentifier(ScopedIdentifier* node) = 0;
-    virtual void visitSelect(Select* node) = 0;
-    virtual void visitSubCall(SubCall* node) = 0;
-    virtual void visitSubReturn(SubReturn* node) = 0;
-    virtual void visitVarDecl(VarDecl* node) = 0;
-    virtual void visitUDTDecl(UDTDecl* node) = 0;
-    virtual void visitUDTDeclBody(UDTDeclBody* node) = 0;
-    virtual void visitUDTFieldAssignment(UDTFieldAssignment* node) = 0;
-    virtual void visitUDTFieldOuter(UDTFieldOuter* node) = 0;
-    virtual void visitUDTFieldInner(UDTFieldInner* node) = 0;
-    virtual void visitUnaryOp(UnaryOp* node) = 0;
-    virtual void visitUntilLoop(UntilLoop* node) = 0;
-    virtual void visitVarAssignment(VarAssignment* node) = 0;
-    virtual void visitVarRef(VarRef* node) = 0;
-    virtual void visitWhileLoop(WhileLoop* node) = 0;
-
-#define X(dbname, cppname) \
-    virtual void visit##dbname##Literal(dbname##Literal* node) = 0;
-    ODB_DATATYPE_LIST
+#define X(nodeType) virtual void visit##nodeType(nodeType* node) = 0;
+    ODB_AST_NODE_TYPE_LIST
 #undef X
 };
 
@@ -112,102 +27,16 @@ class ODBCOMPILER_PUBLIC_API ConstVisitor
 public:
     virtual ~ConstVisitor() = default;
 
-    virtual void visitArgList(const ArgList* node) = 0;
-    virtual void visitArrayAssignment(const ArrayAssignment* node) = 0;
-    virtual void visitArrayDecl(const ArrayDecl* node) = 0;
-    virtual void visitArrayRef(const ArrayRef* node)  = 0;
-    virtual void visitBinaryOp(const BinaryOp* node) = 0;
-    virtual void visitBlock(const Block* node) = 0;
-    virtual void visitCase(const Case* node) = 0;
-    virtual void visitCaseList(const CaseList* node) = 0;
-    virtual void visitCommandExpr(const CommandExpr* node) = 0;
-    virtual void visitCommandStmnt(const CommandStmnt* node) = 0;
-    virtual void visitConditional(const Conditional* node) = 0;
-    virtual void visitConstDecl(const ConstDecl* node) = 0;
-    virtual void visitConstDeclExpr(const ConstDeclExpr* node) = 0;
-    virtual void visitDefaultCase(const DefaultCase* node) = 0;
-    virtual void visitExit(const Exit* node) = 0;
-    virtual void visitForLoop(const ForLoop* node) = 0;
-    virtual void visitFuncCallExpr(const FuncCallExpr* node) = 0;
-    virtual void visitFuncCallExprOrArrayRef(const FuncCallExprOrArrayRef* node)  = 0;
-    virtual void visitFuncCallStmnt(const FuncCallStmnt* node) = 0;
-    virtual void visitFuncDecl(const FuncDecl* node) = 0;
-    virtual void visitFuncExit(const FuncExit* node) = 0;
-    virtual void visitGoto(const Goto* node) = 0;
-    virtual void visitIdentifier(const Identifier* node) = 0;
-    virtual void visitInfiniteLoop(const InfiniteLoop* node) = 0;
-    virtual void visitInitializerList(const InitializerList* node) = 0;
-    virtual void visitLabel(const Label* node) = 0;
-    virtual void visitScopedIdentifier(const ScopedIdentifier* node) = 0;
-    virtual void visitSelect(const Select* node) = 0;
-    virtual void visitSubCall(const SubCall* node) = 0;
-    virtual void visitSubReturn(const SubReturn* node) = 0;
-    virtual void visitVarDecl(const VarDecl* node) = 0;
-    virtual void visitUDTDecl(const UDTDecl* node) = 0;
-    virtual void visitUDTDeclBody(const UDTDeclBody* node) = 0;
-    virtual void visitUDTFieldAssignment(const UDTFieldAssignment* node) = 0;
-    virtual void visitUDTFieldOuter(const UDTFieldOuter* node) = 0;
-    virtual void visitUDTFieldInner(const UDTFieldInner* node) = 0;
-    virtual void visitUnaryOp(const UnaryOp* node) = 0;
-    virtual void visitUntilLoop(const UntilLoop* node) = 0;
-    virtual void visitVarAssignment(const VarAssignment* node) = 0;
-    virtual void visitVarRef(const VarRef* node) = 0;
-    virtual void visitWhileLoop(const WhileLoop* node) = 0;
-
-#define X(dbname, cppname) \
-    virtual void visit##dbname##Literal(const dbname##Literal* node) = 0;
-    ODB_DATATYPE_LIST
+#define X(nodeType) virtual void visit##nodeType(const nodeType* node) = 0;
+    ODB_AST_NODE_TYPE_LIST
 #undef X
 };
 
 class ODBCOMPILER_PUBLIC_API GenericVisitor : public Visitor
 {
 public:
-    void visitArgList(ArgList* node) override;
-    void visitArrayAssignment(ArrayAssignment* node) override;
-    void visitArrayDecl(ArrayDecl* node) override;
-    void visitArrayRef(ArrayRef* node) override;
-    void visitBinaryOp(BinaryOp* node) override;
-    void visitBlock(Block* node) override;
-    void visitCase(Case* node) override;
-    void visitCaseList(CaseList* node) override;
-    void visitCommandExpr(CommandExpr* node) override;
-    void visitCommandStmnt(CommandStmnt* node) override;
-    void visitConditional(Conditional* node) override;
-    void visitConstDecl(ConstDecl* node) override;
-    void visitConstDeclExpr(ConstDeclExpr* node) override;
-    void visitDefaultCase(DefaultCase* node) override;
-    void visitExit(Exit* node) override;
-    void visitForLoop(ForLoop* node) override;
-    void visitFuncCallExpr(FuncCallExpr* node) override;
-    void visitFuncCallExprOrArrayRef(FuncCallExprOrArrayRef* node) override;
-    void visitFuncCallStmnt(FuncCallStmnt* node) override;
-    void visitFuncDecl(FuncDecl* node) override;
-    void visitFuncExit(FuncExit* node) override;
-    void visitGoto(Goto* node) override;
-    void visitIdentifier(Identifier* node) override;
-    void visitInfiniteLoop(InfiniteLoop* node) override;
-    void visitInitializerList(InitializerList* node) override;
-    void visitLabel(Label* node) override;
-    void visitScopedIdentifier(ScopedIdentifier* node) override;
-    void visitSelect(Select* node) override;
-    void visitSubCall(SubCall* node) override;
-    void visitSubReturn(SubReturn* node) override;
-    void visitVarDecl(VarDecl* node) override;
-    void visitUDTDecl(UDTDecl* node) override;
-    void visitUDTDeclBody(UDTDeclBody* node) override;
-    void visitUDTFieldAssignment(UDTFieldAssignment* node) override;
-    void visitUDTFieldOuter(UDTFieldOuter* node) override;
-    void visitUDTFieldInner(UDTFieldInner* node) override;
-    void visitUnaryOp(UnaryOp* node) override;
-    void visitUntilLoop(UntilLoop* node) override;
-    void visitVarAssignment(VarAssignment* node) override;
-    void visitVarRef(VarRef* node) override;
-    void visitWhileLoop(WhileLoop* node) override;
-
-#define X(dbname, cppname) \
-    void visit##dbname##Literal(dbname##Literal* node) override;
-    ODB_DATATYPE_LIST
+#define X(nodeType) void visit##nodeType(nodeType* node) override;
+    ODB_AST_NODE_TYPE_LIST
 #undef X
 
     virtual void visit(Node* node) = 0;
@@ -216,51 +45,8 @@ public:
 class ODBCOMPILER_PUBLIC_API GenericConstVisitor : public ConstVisitor
 {
 public:
-    void visitArgList(const ArgList* node) override;
-    void visitArrayAssignment(const ArrayAssignment* node) override;
-    void visitArrayDecl(const ArrayDecl* node) override;
-    void visitArrayRef(const ArrayRef* node) override;
-    void visitBinaryOp(const BinaryOp* node) override;
-    void visitBlock(const Block* node) override;
-    void visitCase(const Case* node) override;
-    void visitCaseList(const CaseList* node) override;
-    void visitCommandExpr(const CommandExpr* node) override;
-    void visitCommandStmnt(const CommandStmnt* node) override;
-    void visitConditional(const Conditional* node) override;
-    void visitConstDecl(const ConstDecl* node) override;
-    void visitConstDeclExpr(const ConstDeclExpr* node) override;
-    void visitDefaultCase(const DefaultCase* node) override;
-    void visitExit(const Exit* node) override;
-    void visitForLoop(const ForLoop* node) override;
-    void visitFuncCallExpr(const FuncCallExpr* node) override;
-    void visitFuncCallExprOrArrayRef(const FuncCallExprOrArrayRef* node) override;
-    void visitFuncCallStmnt(const FuncCallStmnt* node) override;
-    void visitFuncDecl(const FuncDecl* node) override;
-    void visitFuncExit(const FuncExit* node) override;
-    void visitGoto(const Goto* node) override;
-    void visitIdentifier(const Identifier* node) override;
-    void visitInfiniteLoop(const InfiniteLoop* node) override;
-    void visitInitializerList(const InitializerList* node) override;
-    void visitLabel(const Label* node) override;
-    void visitScopedIdentifier(const ScopedIdentifier* node) override;
-    void visitSelect(const Select* node) override;
-    void visitSubCall(const SubCall* node) override;
-    void visitSubReturn(const SubReturn* node) override;
-    void visitVarDecl(const VarDecl* node) override;
-    void visitUDTDecl(const UDTDecl* node) override;
-    void visitUDTDeclBody(const UDTDeclBody* node) override;
-    void visitUDTFieldOuter(const UDTFieldOuter* node) override;
-    void visitUDTFieldInner(const UDTFieldInner* node) override;
-    void visitUDTFieldAssignment(const UDTFieldAssignment* node) override;
-    void visitUnaryOp(const UnaryOp* node) override;
-    void visitUntilLoop(const UntilLoop* node) override;
-    void visitVarAssignment(const VarAssignment* node) override;
-    void visitVarRef(const VarRef* node) override;
-    void visitWhileLoop(const WhileLoop* node) override;
-
-#define X(dbname, cppname) \
-    void visit##dbname##Literal(const dbname##Literal* node) override;
-    ODB_DATATYPE_LIST
+#define X(nodeType) void visit##nodeType(const nodeType* node) override;
+    ODB_AST_NODE_TYPE_LIST
 #undef X
 
     virtual void visit(const Node* node) = 0;
--- odb-compiler/src/ast/Visitor.cpp
+++ odb-compiler/src/ast/Visitor.cpp
@@ -34,101 +34,16 @@
 namespace odb::ast {
 
 // ----------------------------------------------------------------------------
-void GenericVisitor::visitArgList(ArgList* node)                               { visit(node); }
-void GenericVisitor::visitArrayAssignment(ArrayAssignment* node)               { visit(node); }
-void GenericVisitor::visitArrayDecl(ArrayDecl* node)                           { visit(node); }
-void GenericVisitor::visitArrayRef(ArrayRef* node)                             { visit(node); }
-void GenericVisitor::visitBinaryOp(BinaryOp* node)                             { visit(node); }
-void GenericVisitor::visitBlock(Block* node)                                   { visit(node); }
-void GenericVisitor::visitCase(Case* node)                                     { visit(node); }
-void GenericVisitor::visitCaseList(CaseList* node)                             { visit(node); }
-void GenericVisitor::visitCommandExpr(CommandExpr* node)                       { visit(node); }
-void GenericVisitor::visitCommandStmnt(CommandStmnt* node)                     { visit(node); }
-void GenericVisitor::visitConditional(Conditional* node)                       { visit(node); }
-void GenericVisitor::visitConstDecl(ConstDecl* node)                           { visit(node); }
-void GenericVisitor::visitConstDeclExpr(ConstDeclExpr* node)                   { visit(node); }
-void GenericVisitor::visitDefaultCase(DefaultCase* node)                       { visit(node); }
-void GenericVisitor::visitExit(Exit* node)                                     { visit(node); }
-void GenericVisitor::visitForLoop(ForLoop* node)                               { visit(node); }
-void GenericVisitor::visitFuncCallExpr(FuncCallExpr* node)                     { visit(node); }
-void GenericVisitor::visitFuncCallExprOrArrayRef(FuncCallExprOrArrayRef* node) { visit(node); }
-void GenericVisitor::visitFuncCallStmnt(FuncCallStmnt* node)                   { visit(node); }
-void GenericVisitor::visitFuncDecl(FuncDecl* node)                             { visit(node); }
-void GenericVisitor::visitFuncExit(FuncExit* node)                             { visit(node); }
-void GenericVisitor::visitGoto(Goto* node)                                     { visit(node); }
-void GenericVisitor::visitIdentifier(Identifier* node)                         { visit(node); }
-void GenericVisitor::visitInfiniteLoop(InfiniteLoop* node)                     { visit(node); }
-void GenericVisitor::visitInitializerList(InitializerList* node)               { visit(node); }
-void GenericVisitor::visitLabel(Label* node)                                   { visit(node); }
-void GenericVisitor::visitScopedIdentifier(ScopedIdentifier* node)             { visit(node); }
-void GenericVisitor::visitSelect(Select* node)                                 { visit(node); }
-void GenericVisitor::visitSubCall(SubCall* node)                               { visit(node); }
-void GenericVisitor::visitSubReturn(SubReturn* node)                           { visit(node); }
-void GenericVisitor::visitVarDecl(VarDecl* node)                               { visit(node); }
-void GenericVisitor::visitUDTDecl(UDTDecl* node)                               { visit(node); }
-void GenericVisitor::visitUDTDeclBody(UDTDeclBody* node)                       { visit(node); }
-void GenericVisitor::visitUDTFieldOuter(UDTFieldOuter* node)                   { visit(node); }
-void GenericVisitor::visitUDTFieldInner(UDTFieldInner* node)                   { visit(node); }
-void GenericVisitor::visitUDTFieldAssignment(UDTFieldAssignment* node)         { visit(node); }
-void GenericVisitor::visitUnaryOp(UnaryOp* node)                               { visit(node); }
-void GenericVisitor::visitUntilLoop(UntilLoop* node)                           { visit(node); }
-void GenericVisitor::visitVarAssignment(VarAssignment* node)                   { visit(node); }
-void GenericVisitor::visitVarRef(VarRef* node)                                 { visit(node); }
-void GenericVisitor::visitWhileLoop(WhileLoop* node)                           { visit(node); }
-
-#define X(dbname, cppname) \
-    void GenericVisitor::visit##dbname##Literal(dbname##Literal* node)         { visit(node); }
-ODB_DATATYPE_LIST
+#define X(nodeType) void GenericVisitor::visit##nodeType(nodeType* node) { visit(node); }
+ODB_AST_NODE_TYPE_LIST
 #undef X
 
 // ----------------------------------------------------------------------------
-void GenericConstVisitor::visitArgList(const ArgList* node)                               { visit(node); }
-void GenericConstVisitor::visitArrayAssignment(const ArrayAssignment* node)               { visit(node); }
-void GenericConstVisitor::visitArrayDecl(const ArrayDecl* node)                           { visit(node); }
-void GenericConstVisitor::visitArrayRef(const ArrayRef* node)                             { visit(node); }
-void GenericConstVisitor::visitBinaryOp(const BinaryOp* node)                             { visit(node); }
-void GenericConstVisitor::visitBlock(const Block* node)                                   { visit(node); }
-void GenericConstVisitor::visitCase(const Case* node)                                     { visit(node); }
-void GenericConstVisitor::visitCaseList(const CaseList* node)                             { visit(node); }
-void GenericConstVisitor::visitCommandExpr(const CommandExpr* node)                       { visit(node); }
-void GenericConstVisitor::visitCommandStmnt(const CommandStmnt* node)                     { visit(node); }
-void GenericConstVisitor::visitConditional(const Conditional* node)                       { visit(node); }
-void GenericConstVisitor::visitConstDecl(const ConstDecl* node)                           { visit(node); }
-void GenericConstVisitor::visitConstDeclExpr(const ConstDeclExpr* node)                   { visit(node); }
-void GenericConstVisitor::visitDefaultCase(const DefaultCase* node)                       { visit(node); }
-void GenericConstVisitor::visitExit(const Exit* node)                                     { visit(node); }
-void GenericConstVisitor::visitForLoop(const ForLoop* node)                               { visit(node); }
-void GenericConstVisitor::visitFuncCallExpr(const FuncCallExpr* node)                     { visit(node); }
-void GenericConstVisitor::visitFuncCallExprOrArrayRef(const FuncCallExprOrArrayRef* node) { visit(node); }
-void GenericConstVisitor::visitFuncCallStmnt(const FuncCallStmnt* node)                   { visit(node); }
-void GenericConstVisitor::visitFuncDecl(const FuncDecl* node)                             { visit(node); }
-void GenericConstVisitor::visitFuncExit(const FuncExit* node)                             { visit(node); }
-void GenericConstVisitor::visitGoto(const Goto* node)                                     { visit(node); }
-void GenericConstVisitor::visitIdentifier(const Identifier* node)                         { visit(node); }
-void GenericConstVisitor::visitInfiniteLoop(const InfiniteLoop* node)                     { visit(node); }
-void GenericConstVisitor::visitInitializerList(const InitializerList* node)               { visit(node); }
-void GenericConstVisitor::visitLabel(const Label* node)                                   { visit(node); }
-void GenericConstVisitor::visitScopedIdentifier(const ScopedIdentifier* node)             { visit(node); }
-void GenericConstVisitor::visitSelect(const Select* node)                                 { visit(node); }
-void GenericConstVisitor::visitSubCall(const SubCall* node)                               { visit(node); }
-void GenericConstVisitor::visitSubReturn(const SubReturn* node)                           { visit(node); }
-void GenericConstVisitor::visitVarDecl(const VarDecl* node)                               { visit(node); }
-void GenericConstVisitor::visitUDTDecl(const UDTDecl* node)                               { visit(node); }
-void GenericConstVisitor::visitUDTDeclBody(const UDTDeclBody* node)                       { visit(node); }
-void GenericConstVisitor::visitUDTFieldOuter(const UDTFieldOuter* node)                   { visit(node); }
-void GenericConstVisitor::visitUDTFieldInner(const UDTFieldInner* node)                   { visit(node); }
-void GenericConstVisitor::visitUDTFieldAssignment(const UDTFieldAssignment* node)         { visit(node); }
-void GenericConstVisitor::visitUnaryOp(const UnaryOp* node)                               { visit(node); }
-void GenericConstVisitor::visitUntilLoop(const UntilLoop* node)                           { visit(node); }
-void GenericConstVisitor::visitVarAssignment(const VarAssignment* node)                   { visit(node); }
-void GenericConstVisitor::visitVarRef(const VarRef* node)                                 { visit(node); }
-void GenericConstVisitor::visitWhileLoop(const WhileLoop* node)                           { visit(node); }
-
-#define X(dbname, cppname) \
-    void GenericConstVisitor::visit##dbname##Literal(const dbname##Literal* node)         { visit(node); }
-ODB_DATATYPE_LIST
+#define X(nodeType) void GenericConstVisitor::visit##nodeType(const nodeType* node) { visit(node); }
+ODB_AST_NODE_TYPE_LIST
 #undef X
 
+// ----------------------------------------------------------------------------
 void visitAST(Node* node, Visitor& visitor, Traversal traversal)
 {
     switch (traversal)
@@ -150,6 +65,7 @@ void visitAST(Node* node, Visitor& visitor, Traversal traversal)
     }
 }
 
+// ----------------------------------------------------------------------------
 void visitAST(const Node* node, ConstVisitor& visitor, Traversal traversal)
 {
     switch (traversal)
