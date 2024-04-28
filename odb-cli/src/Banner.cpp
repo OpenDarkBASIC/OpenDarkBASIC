b679eeed885c88342c34fec567dbfb823da48726
TheComet <alex.murray@gmx.ch>
2024-04-28 17:55:37 +0200

    Shortening banner

--- odb-cli/src/Banner.cpp
+++ odb-cli/src/Banner.cpp
@@ -31,7 +31,7 @@ extern "C" {
 #define VERSION_NUMBER B_CYAN
 
 static bool printBanner_ = true;
-static const char* bannerNoEscapeSequences_ =
+static const char* widerBannerNoEscapeSequences_ =
 R"(              ▄▀▀█
             ▄▀`╫╫╠▀█
           ▄▀░"/╠╢╟▓▒▀█,               ____                   _____             _    ____           _____ _____ _____
@@ -46,7 +46,7 @@ R"(              ▄▀▀█
         ▀█▀▀▀▀`                      Version %s (%s)
 
 )";
-static const char* banner_ =
+static const char* wideBanner_ =
    PYRAMID_LEFT R"(              ▄▀)" PYRAMID_RIGHT R"(▀█
 )" PYRAMID_LEFT R"(            ▄▀`╫)" PYRAMID_RIGHT R"(╫╠▀█
 )" PYRAMID_LEFT R"(          ▄▀░"/)" PYRAMID_RIGHT R"(╠╢╟▓▒▀█,             )" TEXT       R"(  ____                   _____             _    ____           _____ _____ _____
@@ -61,7 +61,107 @@ static const char* banner_ =
 )" PYRAMID_LEFT R"(        ▀█▀)" PYRAMID_RIGHT R"(▀▀▀`                     )" VERSION_TEXT " Version " VERSION_NUMBER "%s " VERSION_TEXT "(" VERSION_NUMBER "%s" VERSION_TEXT R"()
 )" RESET        R"(
 )";
+static const char* bannerNoEscapeSequences_ =
+R"(              ▄▀▀█
+            ▄▀`╫╫╠▀█
+          ▄▀░"/╠╢╟▓▒▀█,
+        ▄█▓▒░░╨╢R▒▓▓▌▒▀█,             ___                   ____  ____  ____
+      ▄▀Å▀▓╬░]╗φ╫▀T▀▀▀██▀█,          / _ \ _ __   ___ _ __ |  _ \| __ )|  _ \
+    ▄▀.┤D╠╬7┴j╟å J▒─▀█▄▀████,       | | | | '_ \ / _ \ '_ \| | | |  _ \| |_) |
+  ▄▀ ^j]╚DD░÷╠╠╣~`╓▄▄▌▄▄██████,     | |_| | |_) |  __/ | | | |_| | |_) |  __/
+╓▀   ^░░░ß░Ü<║╫▓▓███████████████,    \___/| .__/ \___|_| |_|____/|____/|_|
+▐█▄  ~░─╚░*U⌐Å▒▒█████▀████████████,       |_|
+  `██▄─ %.='╦╢╫▌█████▌▄▐██████████▌
+     ▀██░⌂r3▄▒▓█████████▀▀└          %s
+        ▀█▀▀▀▀`                      Version %s (%s)
 
+)";
+static const char* banner_ =
+   PYRAMID_LEFT R"(              ▄▀)" PYRAMID_RIGHT R"(▀█
+)" PYRAMID_LEFT R"(            ▄▀`╫)" PYRAMID_RIGHT R"(╫╠▀█
+)" PYRAMID_LEFT R"(          ▄▀░"/)" PYRAMID_RIGHT R"(╠╢╟▓▒▀█,             )" TEXT       R"(
+)" PYRAMID_LEFT R"(        ▄█▓▒░░╨)" PYRAMID_RIGHT R"(╢R▒▓▓▌▒▀█,           )" TEXT       R"(  ___                   ____  ____  ____
+)" PYRAMID_LEFT R"(      ▄▀Å▀▓╬░])" PYRAMID_RIGHT R"(╗φ╫▀T▀▀▀██▀█,         )" TEXT       R"( / _ \ _ __   ___ _ __ |  _ \| __ )|  _ \
+)" PYRAMID_LEFT R"(    ▄▀.┤D╠╬7┴j)" PYRAMID_RIGHT R"(╟å J▒─▀█▄▀████,       )" TEXT       R"(| | | | '_ \ / _ \ '_ \| | | |  _ \| |_) |
+)" PYRAMID_LEFT R"(  ▄▀ ^j]╚DD░÷)" PYRAMID_RIGHT R"(╠╠╣~`╓▄▄▌▄▄██████,     )" TEXT       R"(| |_| | |_) |  __/ | | | |_| | |_) |  __/
+)" PYRAMID_LEFT R"(╓▀   ^░░░ß░Ü<)" PYRAMID_RIGHT R"(║╫▓▓███████████████,   )" TEXT       R"( \___/| .__/ \___|_| |_|____/|____/|_|
+)" PYRAMID_LEFT R"(▐█▄  ~░─╚░*U)" PYRAMID_RIGHT R"(⌐Å▒▒█████▀████████████, )" TEXT       R"(      |_|
+)" PYRAMID_LEFT R"(  `██▄─ %.=')" PYRAMID_RIGHT R"(╦╢╫▌█████▌▄▐██████████▌
+)" PYRAMID_LEFT R"(     ▀██░⌂r)" PYRAMID_RIGHT R"(3▄▒▓█████████▀▀└         )" URL        R"( %s
+)" PYRAMID_LEFT R"(        ▀█▀)" PYRAMID_RIGHT R"(▀▀▀`                     )" VERSION_TEXT " Version " VERSION_NUMBER "%s " VERSION_TEXT "(" VERSION_NUMBER "%s" VERSION_TEXT R"()
+)" RESET        R"(
+)";
+
+static const char* smallBannerNoEscapeSequences_ =
+R"(              ▄▀▀█
+            ▄▀`╫╫╠▀█
+          ▄▀░"/╠╢╟▓▒▀█,
+        ▄█▓▒░░╨╢R▒▓▓▌▒▀█,
+      ▄▀Å▀▓╬░]╗φ╫▀T▀▀▀██▀█,
+    ▄▀.┤D╠╬7┴j╟å J▒─▀█▄▀████,
+  ▄▀ ^j]╚DD░÷╠╠╣~`╓▄▄▌▄▄██████,
+╓▀   ^░░░ß░Ü<║╫▓▓███████████████,
+▐█▄  ~░─╚░*U⌐Å▒▒█████▀████████████,
+  `██▄─ %.='╦╢╫▌█████▌▄▐██████████▌
+     ▀██░⌂r3▄▒▓█████████▀▀└
+        ▀█▀▀▀▀`
+%s
+Version %s (%s)
+
+)";
+static const char* smallBanner_ =
+   PYRAMID_LEFT R"(              ▄▀)" PYRAMID_RIGHT R"(▀█
+)" PYRAMID_LEFT R"(            ▄▀`╫)" PYRAMID_RIGHT R"(╫╠▀█
+)" PYRAMID_LEFT R"(          ▄▀░"/)" PYRAMID_RIGHT R"(╠╢╟▓▒▀█,
+)" PYRAMID_LEFT R"(        ▄█▓▒░░╨)" PYRAMID_RIGHT R"(╢R▒▓▓▌▒▀█,
+)" PYRAMID_LEFT R"(      ▄▀Å▀▓╬░])" PYRAMID_RIGHT R"(╗φ╫▀T▀▀▀██▀█,
+)" PYRAMID_LEFT R"(    ▄▀.┤D╠╬7┴j)" PYRAMID_RIGHT R"(╟å J▒─▀█▄▀████,
+)" PYRAMID_LEFT R"(  ▄▀ ^j]╚DD░÷)" PYRAMID_RIGHT R"(╠╠╣~`╓▄▄▌▄▄██████,
+)" PYRAMID_LEFT R"(╓▀   ^░░░ß░Ü<)" PYRAMID_RIGHT R"(║╫▓▓███████████████,
+)" PYRAMID_LEFT R"(▐█▄  ~░─╚░*U)" PYRAMID_RIGHT R"(⌐Å▒▒█████▀████████████,
+)" PYRAMID_LEFT R"(  `██▄─ %.=')" PYRAMID_RIGHT R"(╦╢╫▌█████▌▄▐██████████▌
+)" PYRAMID_LEFT R"(     ▀██░⌂r)" PYRAMID_RIGHT R"(3▄▒▓█████████▀▀└
+)" PYRAMID_LEFT R"(        ▀█▀)" PYRAMID_RIGHT R"(▀▀▀`
+)" URL          R"(%s
+)" VERSION_TEXT   "Version " VERSION_NUMBER "%s " VERSION_TEXT "(" VERSION_NUMBER "%s" VERSION_TEXT R"()
+)" RESET        R"(
+)";
+
+/*
+   ____  _____  ____
+  / __ \|  __ \|  _ \
+ | |  | | |  | | |_) |
+ | |  | | |  | |  _ <
+ | |__| | |__| | |_) |
+  \____/|_____/|____/
+
+   ____                   _____  ____  _____
+  / __ \                 |  __ \|  _ \|  __ \
+ | |  | |_ __   ___ _ __ | |  | | |_) | |__) |
+ | |  | | '_ \ / _ \ '_ \| |  | |  _ <|  ___/
+ | |__| | |_) |  __/ | | | |__| | |_) | |
+  \____/| .__/ \___|_| |_|_____/|____/|_|
+        | |
+        |_|
+
+ _____             ____  _____ _____
+|     |___ ___ ___|    \| __  |  _  |
+|  |  | . | -_|   |  |  | __ -|   __|
+|_____|  _|___|_|_|____/|_____|__|
+      |_|
+
+   ___                 ___  ___ ___
+  / _ \ _ __  ___ _ _ |   \| _ ) _ \
+ | (_) | '_ \/ -_) ' \| |) | _ \  _/
+  \___/| .__/\___|_||_|___/|___/_|
+       |_|
+   ___                   ____  ____  ____
+  / _ \ _ __   ___ _ __ |  _ \| __ )|  _ \
+ | | | | '_ \ / _ \ '_ \| | | |  _ \| |_) |
+ | |_| | |_) |  __/ | | | |_| | |_) |  __/
+  \___/| .__/ \___|_| |_|____/|____/|_|
+       |_|
+*/
 // ----------------------------------------------------------------------------
 static void printBannerNormal()
 {
