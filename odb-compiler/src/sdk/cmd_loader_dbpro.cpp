6e3ab1db6daf47cbd68b87bdc288df0a1d6fad36
TheComet <alex.murray@gmx.ch>
2024-05-12 10:09:29 +0200

    Split command name

--- odb-compiler/src/sdk/cmd_loader_dbpro.cpp
+++ odb-compiler/src/sdk/cmd_loader_dbpro.cpp
@@ -17,7 +17,7 @@ load_dbpro_commands(
     if (pe->resources() == nullptr)
     {
         log_sdk_warn(
-            "No resources found in plugin {quote:%s}. Skipping...\n",
+            "No resources found in plugin {quote:%s}.\n",
             ospath_view_cstr(filepath));
         return 0;
     }
@@ -34,7 +34,7 @@ load_dbpro_commands(
     if (stringTableNodeIt == std::end(nodes))
     {
         log_sdk_warn(
-            "Missing string table in plugin {quote:%s}. Skipping...\n",
+            "Missing string table in plugin {quote:%s}.\n",
             ospath_view_cstr(filepath));
         return 0;
     }
@@ -42,22 +42,33 @@ load_dbpro_commands(
         || stringTableNodeIt->childs()[0].childs().size() == 0)
     {
         log_sdk_warn(
-            "Malformed string table structure in plugin {quote:%s}. "
-            "Skipping...\n",
+            "Malformed string table structure in plugin {quote:%s}.\n",
             ospath_view_cstr(filepath));
         return 0;
     }
 
-    struct utf8 name = empty_utf8();
+    struct utf8 entry = empty_utf8();
     if (auto resmgr = pe->resources_manager())
         for (const auto& entry : resmgr.value().string_table())
         {
             const std::u16string& u16 = entry.name();
             struct utf16_view     u16v
                 = {(const uint16_t*)u16.data(), (utf16_idx)u16.length()};
-            if (utf16_to_utf8(&name, u16v) != 0)
+            if (utf16_to_utf8(&entry, u16v) != 0)
             {
-                utf8_deinit(name);
+                utf8_deinit(entry);
+                return 0;
+            }
+
+            struct utf8_view cmd_name, type_str;
+            utf8_split(utf8_view(entry), '%' &cmd_name, &type_str);
+            if (cmd_name.len == 0)
+            {
+                log_sdk_warn(
+                    "Invalid string table entry in plugin {quote:%s}: {emph:%s}\n",
+                    ospath_view_cstr(filepath),
+                    utf8_cstr(entry));
+                utf8_deinit(entry);
                 return 0;
             }
 
@@ -65,16 +76,16 @@ load_dbpro_commands(
                     commands,
                     0,
                     CMD_ARG_VOID,
-                    utf8_view(name),
+                    utf8_view(entry),
                     empty_utf8_view(),
                     empty_utf8_view())
                 < 0)
             {
-                utf8_deinit(name);
+                utf8_deinit(entry);
                 return -1;
             }
         }
-    utf8_deinit(name);
+    utf8_deinit(entry);
 
     return 1;
 }
