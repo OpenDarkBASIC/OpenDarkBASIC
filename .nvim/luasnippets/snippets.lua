246da5a018927ddd66258f6e2c8182a3e55db615
TheComet <alex.murray@gmx.ch>
2024-11-12 01:37:30 +0100

    Working on tooling

--- .nvim/luasnippets/snippets.lua
+++ .nvim/luasnippets/snippets.lua
@@ -1,7 +1,108 @@
-return {
-    s({ filetype = "cpp", trig = "TEST", desc = "Add a unit test" }, {
-        t("TEST_F(NAME, "),
-        i(1),
-        t(")"),
+local ls = require("luasnip")
+local extras = require("luasnip.extras")
+local fmt = require("luasnip.extras.fmt").fmt
+local sn = ls.snippet
+local c = ls.choice_node
+local i = ls.insert_node
+local r = ls.restore_node
+local t = ls.text_node
+local f = ls.function_node
+local k = require("luasnip.nodes.key_indexer").new_key
+local d = ls.dynamic_node
+local rep = extras.rep
+
+local function var_to_node_name(value)
+    local special_cases = {
+        ["ident"] = "identifier",
+        ["ass"] = "assignment",
+    }
+    return special_cases[value:gsub("[%d_]+$", "")] or value
+end
+
+ls.add_snippets("c", {
+    sn({ trig = "ast", desc = "Access AST node" },
+        fmt("{}->nodes[{}].{}.{};", {
+            c(1, { i(1, "ast"), i(2, "(*astp)") }),
+            i(2, "node", { key = "varname" }),
+            c(3, {
+                f(function(values) return var_to_node_name(values[1][1]) end, k("varname")),
+                i(2, "node"),
+            }),
+            i(4, "property"),
+        })
+    ),
+    sn({ trig = "asd", desc = "Access AST node" },
+        -- In C the variable is usually declared at the top of the function
+        fmt("{} = {}->nodes[{}].{}.{};", {
+            i(1, "node", { key = "varname" }),
+            c(2, { i(1, "ast"), i(2, "(*astp)") }),
+            i(3, "parent"),
+            c(4, {
+                f(function(values) return var_to_node_name(values[1][1]) end, k("varname")),
+                i(2, "node"),
+            }),
+            i(5, "property")
+        })
+    ),
+    sn({ trig = "log", docstring = "Logging function" }, {
+        c(1, { i(1, "log_err"), i(2, "log_warn"), i(3, "log_dbg"), i(4, "log_info") }),
+        t('("'), i(2, ""), t('\\n"'),
+        d(3, function(values)
+            local fmt_string = values[1][1]
+            local nodes = {}
+            for _ in fmt_string:gmatch("%%[^%%]") do
+                local idx = #nodes / 2 + 1
+                table.insert(nodes, t(", "))
+                table.insert(nodes, r(idx, "arg" .. idx, i(nil, "arg" .. idx)))
+            end
+            return sn({ docstring = "" }, nodes)
+        end, { 2 }),
+        t(");"),
     }),
-}
+    sn({ trig = "asta", docstring = "Assert AST nodes" }, {
+        t("ODBUTIL_DEBUG_ASSERT("),
+        c(1, {t("ast_node_type"), t("ast_type_info"),}),
+        t("("), c(2, { i(1, "ast"), i(2, "*astp") }),
+        t(", "), i(3, "node"), t(")"),
+        t(" == "),
+        f(function(values)
+            if values[1][1] == "ast_node_type" then return "AST_" else return "TYPE_" end
+        end, {1}),
+        i(4, "BLOCK"),
+        t(", log_err("),
+        f(function(values)
+            local func = values[1][1]
+            local ast = values[2][1]
+            local node = values[3][1]
+            return '"type: %d\\n", ' .. func .. "(" .. ast .. ", " .. node .. ")"
+        end, { 1, 2, 3 }),
+        t("));"),
+    }),
+}, { key = "OpenDarkBASIC-c" })
+
+ls.add_snippets("cpp", {
+    sn({ trig = "ast", desc = "Access AST node" },
+        fmt("{}->nodes[{}].{}.{};", {
+            c(1, { i(1, "ast"), i(2, "(*astp)") }),
+            i(2, "node", { key = "varname" }),
+            c(3, {
+                f(function(values) return var_to_node_name(values[1][1]) end, k("varname")),
+                i(2, "node"),
+            }),
+            i(4, "property"),
+        })
+    ),
+    sn({ trig = "asd", desc = "Define and access AST node" },
+        -- In C++ the variable is usually declared at the same location
+        fmt("ast_id {} = {}->nodes[{}].{}.{};", {
+            i(1, "node"),
+            c(2, { i(1, "ast"), i(2, "(*astp)") }),
+            i(3, "node", { key = "varname" }),
+            c(4, {
+                f(function(values) return var_to_node_name(values[1][1]) end, k("varname")),
+                i(2, "node"),
+            }),
+            i(5, "property")
+        })
+    ),
+}, { key = "OpenDarkBASIC-cpp" })
--- odb-compiler/tests/src/semantic/test_odbcompiler_semantic_type_check_loop_cont.cpp
+++ odb-compiler/tests/src/semantic/test_odbcompiler_semantic_type_check_loop_cont.cpp
@@ -25,21 +25,45 @@ TEST_F(NAME, continue_with_step)
     ASSERT_THAT(parse(source), Eq(0)) << log().text;
     ASSERT_THAT(semantic(&semantic_type_check), Eq(0)) << log().text;
 
-    ast_id init = ast->root;
-    ast_id loop_block = ast->nodes[init].block.next;
-    ast_id loop1 = ast->nodes[loop_block].block.stmt;
-    ast_id loop2 = ast->nodes[loop1].loop1.loop2;
-    ast_id exit_block = ast->nodes[loop2].loop2.body;
-    ast_id cont_block = ast->nodes[exit_block].block.next;
-    ast_id cont_stmt = ast->nodes[cont_block].block.stmt;
-    ast_id cont_step_block = ast->nodes[cont_stmt].cont.step;
-    ast_id cont_step = ast->nodes[cont_step_block].block.stmt;
-    ASSERT_THAT(ast_node_type(ast, cont_step), Eq(AST_ASSIGNMENT));
-    ast_id cont_step_op = ast->nodes[cont_step].assignment.expr;
-    ASSERT_THAT(ast_type_info(ast, cont_step_op), Eq(TYPE_I32));
-    ast_id cont_step_expr = ast->nodes[cont_step_op].binop.right;
-    ASSERT_THAT(ast_node_type(ast, cont_step_expr), Eq(AST_CAST));
-    ASSERT_THAT(ast_type_info(ast, cont_step_expr), Eq(TYPE_I32));
-    ast_id cont_step_value = ast->nodes[cont_step_expr].cast.expr;
-    ASSERT_THAT(ast->nodes[cont_step_value].byte_literal.value, Eq(2));
+    /* odb-asttool --format gtest --node-filter cont,literal --types
+     * --node-properties */
+    ASSERT_THAT(ast_count(ast), Eq(43));
+
+    ast_id block15 = ast->root;
+    ast_id block8 = ast->nodes[block15].block.next;
+    ast_id loop1_13 = ast->nodes[block8].block.stmt;
+    ast_id loop2_14 = ast->nodes[loop1_13].loop1.loop2;
+    ast_id block9 = ast->nodes[loop2_14].loop2.post_body;
+    ast_id ass11 = ast->nodes[block9].block.stmt;
+    ast_id binop12 = ast->nodes[ass11].assignment.expr;
+    ast_id cast42 = ast->nodes[binop12].binop.right;
+    ast_id lit25 = ast->nodes[cast42].cast.expr;
+    ast_id block10 = ast->nodes[loop2_14].loop2.body;
+    ast_id block7 = ast->nodes[block10].block.next;
+    ast_id cont6 = ast->nodes[block7].block.stmt;
+    ast_id block34 = ast->nodes[cont6].cont.step;
+    ast_id ass33 = ast->nodes[block34].block.stmt;
+    ast_id binop32 = ast->nodes[ass33].assignment.expr;
+    ast_id cast40 = ast->nodes[binop32].binop.right;
+    ast_id lit5 = ast->nodes[cast40].cast.expr;
+    ast_id cond22 = ast->nodes[block10].block.stmt;
+    ast_id binop21 = ast->nodes[cond22].cond.expr;
+    ast_id cast38 = ast->nodes[binop21].binop.right;
+    ast_id lit4 = ast->nodes[cast38].cast.expr;
+    ast_id decl13 = ast->nodes[block15].block.stmt;
+    ast_id cast36 = ast->nodes[decl13].var_decl1.init_expr;
+    ast_id lit2 = ast->nodes[cast36].cast.expr;
+
+    ASSERT_THAT(ast->nodes[lit2].byte_literal.value, Eq(1));
+    ASSERT_THAT(ast->nodes[lit4].byte_literal.value, Eq(5));
+    ASSERT_THAT(ast->nodes[lit5].byte_literal.value, Eq(2));
+    ASSERT_THAT(ast->nodes[cont6].cont.name, Utf8SpanEq(0, 0));
+    ASSERT_THAT(ast->nodes[lit25].byte_literal.value, Eq(1));
+
+    ASSERT_THAT(ast_type_info(ast, lit2), Eq(TYPE_U8));
+    ASSERT_THAT(ast_type_info(ast, lit4), Eq(TYPE_U8));
+    ASSERT_THAT(ast_type_info(ast, lit5), Eq(TYPE_U8));
+    ASSERT_THAT(ast_type_info(ast, cont6), Eq(TYPE_VOID));
+    ASSERT_THAT(ast_type_info(ast, lit25), Eq(TYPE_U8));
+    /* odb-asttool end */
 }
